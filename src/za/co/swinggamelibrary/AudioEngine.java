fca3e1dfc1073aa1b7249a034acd9e6b40aaced5
David Kroukamp <dkroukamp92@gmail.com>
2020-12-19 22:03:48 +0200

    AudioEngine completed

--- src/za/co/swinggamelibrary/AudioEngine.java
+++ src/za/co/swinggamelibrary/AudioEngine.java
@@ -5,18 +5,14 @@
  */
 package za.co.swinggamelibrary;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import kuusisto.tinysound.Music;
 import kuusisto.tinysound.Sound;
 import kuusisto.tinysound.TinySound;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  *
@@ -28,10 +24,14 @@ public class AudioEngine {
 
     HashMap<String, Sound> sounds;
     HashMap<URL, String> cachedSounds;
+    HashMap<String, Music> music;
+    HashMap<URL, String> cachedMusic;
 
     private AudioEngine() {
         sounds = new HashMap<>();
         cachedSounds = new HashMap<>();
+        music = new HashMap<>();
+        cachedMusic = new HashMap<>();
     }
 
     public static AudioEngine getInstance() {
@@ -43,49 +43,61 @@ public class AudioEngine {
         return single_instance;
     }
 
-    public String playSound(URL fileUrl, float volume) throws LineUnavailableException, UnsupportedAudioFileException, IOException, URISyntaxException {
-        String audioId = cache(fileUrl);
+    public String playSound(URL fileUrl, float volume) {
+        String audioId = cacheSound(fileUrl);
         sounds.get(audioId).play(volume);
         return audioId;
     }
 
-    public String playMusic(URL fileUrl, boolean loop, float volume) throws LineUnavailableException, UnsupportedAudioFileException, IOException, URISyntaxException {
-        throw new NotImplementedException();
+    public String playMusic(URL fileUrl, boolean loop, float volume) {
+        String audioId = cacheMusic(fileUrl);
+        music.get(audioId).play(loop, volume);
+        return audioId;
     }
 
-    /* Only relevant for music
-    public void pause(String audioId) {
-        sounds.get(audioId).stop();
+    public void pauseMusic(String audioId) {
+        music.get(audioId).pause();
     }
 
-    public void pauseAll() {
-        sounds.entrySet().forEach((entry) -> {
-            entry.getValue().stop();
-        });
-    }
-     */
-    public void stop(String audioId) {
-        sounds.get(audioId).stop();
-    }
-
-    public void stopAll() {
-        sounds.entrySet().stream().map((entry) -> entry.getValue()).map((clip) -> {
-            clip.stop();
-            return clip;
+    public void pauseAllMusic() {
+        music.entrySet().forEach((entry) -> {
+            entry.getValue().pause();
         });
     }
 
-    public void resume(String audioId) {
-        sounds.get(audioId).stop();
+    public void resumeMusic(String audioId) {
+        music.get(audioId).resume();
     }
 
-    public void resumeAll() {
-        sounds.entrySet().forEach((entry) -> {
-            entry.getValue().play();
+    public void resumeAllMusic() {
+        music.entrySet().forEach((entry) -> {
+            entry.getValue().resume();
         });
     }
 
-    public String cache(URL fileUrl) throws LineUnavailableException, UnsupportedAudioFileException, IOException, URISyntaxException {
+    public void stopMusic(String audioId) {
+        music.get(audioId).stop();
+    }
+
+    public void stopAllMusic() {
+        music.entrySet().stream().map((entry) -> entry.getValue()).map((m) -> {
+            m.stop();
+            return m;
+        });
+    }
+
+    public void stopSound(String audioId) {
+        sounds.get(audioId).stop();
+    }
+
+    public void stopAllSounds() {
+        sounds.entrySet().stream().map((entry) -> entry.getValue()).map((sound) -> {
+            sound.stop();
+            return sound;
+        });
+    }
+
+    public String cacheSound(URL fileUrl) {
         String cachedAudioId = cachedSounds.get(fileUrl);
         if (cachedAudioId == null) {
             String audioId = UUID.randomUUID().toString();
@@ -98,8 +110,8 @@ public class AudioEngine {
         return cachedAudioId;
     }
 
-    public void uncache(String audioId) {
-        stop(audioId);
+    public void uncacheSound(String audioId) {
+        stopSound(audioId);
         sounds.get(audioId).unload();
         sounds.remove(audioId);
         // remove from cachedSounds
@@ -113,7 +125,7 @@ public class AudioEngine {
         }
     }
 
-    public void uncacheAll() {
+    public void uncacheAllSounds() {
         Iterator<Map.Entry<String, Sound>> soundsIterator = sounds.entrySet().iterator();
         while (soundsIterator.hasNext()) {
             Map.Entry<String, Sound> soundEntry = soundsIterator.next();
@@ -129,5 +141,56 @@ public class AudioEngine {
                 }
             }
         }
+    }
+
+    public String cacheMusic(URL fileUrl) {
+        String cachedAudioId = cachedMusic.get(fileUrl);
+        if (cachedAudioId == null) {
+            String audioId = UUID.randomUUID().toString();
+            Music m = TinySound.loadMusic(fileUrl);
+            music.put(audioId, m);
+            cachedMusic.put(fileUrl, audioId);
+            return audioId;
+        }
+
+        return cachedAudioId;
+    }
+
+    public void uncacheMusic(String audioId) {
+        stopMusic(audioId);
+        music.get(audioId).unload();
+        music.remove(audioId);
+        // remove from cachedSounds
+        Iterator<Map.Entry<URL, String>> cachedMusicIterator = cachedMusic.entrySet().iterator();
+        while (cachedMusicIterator.hasNext()) {
+            Map.Entry<URL, String> cachedMusicEntry = cachedMusicIterator.next();
+            if (cachedMusicEntry.getValue().equals(audioId)) {
+                cachedMusicIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public void uncacheAllMusic() {
+        Iterator<Map.Entry<String, Music>> musicIterator = music.entrySet().iterator();
+        while (musicIterator.hasNext()) {
+            Map.Entry<String, Music> musicEntry = musicIterator.next();
+            musicEntry.getValue().unload();
+            musicIterator.remove();
+            // remove from cachedSounds
+            Iterator<Map.Entry<URL, String>> cachedSoundsIterator = cachedMusic.entrySet().iterator();
+            while (cachedSoundsIterator.hasNext()) {
+                Map.Entry<URL, String> cachedSoundEntry = cachedSoundsIterator.next();
+                if (cachedSoundEntry.getValue().equals(musicEntry.getKey())) {
+                    cachedSoundsIterator.remove();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void uncacheAll() {
+        uncacheAllMusic();
+        uncacheAllSounds();
     }
 }
