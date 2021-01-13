45d7c9235008554267142ef6201038a6fbb9eb0b
David Kroukamp <dkroukamp92@gmail.com>
2021-01-13 13:11:45 +0200

    Added flip x on Sprite

--- src/za/co/swinggamelibrary/Sprite.java
+++ src/za/co/swinggamelibrary/Sprite.java
@@ -14,12 +14,14 @@ import java.awt.Graphics2D;
 public class Sprite extends Node {
 
     private final Animator animation;
+    private boolean isFlippedX;
 
     public Sprite(int worldX, int worldY, Animation animation) {
         super(worldX, worldY, 0, 0);
         this.animation = new Animator(animation);
         this.setWidth(this.animation.getCurrentImage().getWidth());
         this.setHeight(this.animation.getCurrentImage().getHeight());
+        this.setFlippedX(false);
     }
 
     @Override
@@ -30,13 +32,30 @@ public class Sprite extends Node {
 
     @Override
     public void render(Graphics2D g2d) {
-        g2d.drawImage(this.animation.getCurrentImage(), (int) this.getScreenX(), (int) this.getScreenY(), null);
+        if (this.isFlippedX) {
+            // flip horizontally
+            g2d.drawImage(this.animation.getCurrentImage(), (int) this.getScreenX() + (int) this.getWidth(), (int) this.getScreenY(), (int) -this.getWidth(), (int) this.getHeight(), null);
+        } else {
+            g2d.drawImage(this.animation.getCurrentImage(), (int) this.getScreenX(), (int) this.getScreenY(), null);
+        }
+
+        // flip it vertically i.e. isFlippedY
+        
+        // g2.drawImage(image, x, y + height, width, -height, null);
         super.render(g2d);
     }
 
     @Override
     public void onEnter() {
         this.animation.start();
+    }
+
+    public void setFlippedX(boolean isFlippedX) {
+        this.isFlippedX = isFlippedX;
+    }
+
+    public boolean getFlippedX() {
+        return this.isFlippedX;
     }
 
 }
